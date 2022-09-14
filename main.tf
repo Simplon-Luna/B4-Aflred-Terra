3c3e54fe6e674ed4911113ae74772d358ee8bc41
Simplon-Luna <108002178+Simplon-Luna@users.noreply.github.com>
2022-09-14 13:41:10 +0200

    New Dun Key

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_ssh_public_key" "ssh_key3" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   public_key          = file("ssh/ssh3.pub") 
-}
+} 
 
 ## Creation SubNet
 resource "azurerm_subnet" "subnet_gateway" {
