728deb49757707354c36b4d764f02ffdda283450
Simplon-Luna <108002178+Simplon-Luna@users.noreply.github.com>
2022-09-14 15:06:07 +0200

    pouet

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_application_gateway" "gateway" {
 ## AppGateway PoolEnd
 resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "poolbackend" {
  network_interface_id    = azurerm_network_interface.nic_app.id
- ip_configuration_name   = "nic_app_config"
+ ip_configuration_name   = "${var.prefix}nic_app_config"
  backend_address_pool_id = tolist(azurerm_application_gateway.gateway.backend_address_pool).0.id
 }
 
