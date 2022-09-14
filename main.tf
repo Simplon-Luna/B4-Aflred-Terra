6deb37381dc6c893fd00081a4fe18aae6db1b1d6
Simplon-Luna <108002178+Simplon-Luna@users.noreply.github.com>
2022-09-14 11:45:52 +0200

    bloop

---
+++ main.tf
@@ -0,0 +1,29 @@
+# Resource group name creation
+resource "random_pet" "rg_name" {
+  prefix = var.rg
+}
+
+# Resource group creation
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.location
+}
+
+resource "azurerm_resource_group" "rg" {
+  name      = var.rg
+  location  = var.location
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = "vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_ssh_public_key" "ssh_key" {
+  name                = "ssh_key_admin"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  public_key          = file("~/.ssh/id_rsa.pub") 
+}
