8f4b649dad5599fa14362bf554906b36b7896356
Simplon-Luna <108002178+Simplon-Luna@users.noreply.github.com>
2022-09-15 12:27:00 +0200

    named

--- main.tf
+++ main.tf
@@ -119,11 +119,11 @@ resource "azurerm_application_gateway" "gateway" {
 }
 
 ## AppGateway PoolEnd
-resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "poolbackend" {
- network_interface_id    = azurerm_network_interface.nic_app.id
- ip_configuration_name   = "${var.prefix}nic_app_config"
- backend_address_pool_id = tolist(azurerm_application_gateway.gateway.backend_address_pool).0.id
-}
+#*#* resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "poolbackend" {
+#*#*  network_interface_id    = azurerm_network_interface.nic_app.id
+#*#*  ip_configuration_name   = "${var.prefix}nic_app_config"
+#*#*  backend_address_pool_id = tolist(azurerm_application_gateway.gateway.backend_address_pool).0.id
+#*#* }
 
 ## NAT
     # Creation NAT PubIP
@@ -294,7 +294,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "scalevmss" {
       name      = "${var.prefix}IPConfiguration"
       primary   = true
       subnet_id = azurerm_subnet.subnet_app.id
-      application_gateway_backend_address_pool_ids = [azurerm_application_gateway.gateway.backend_address_pool[0].id]
+      application_gateway_backend_address_pool_ids = [azurerm_application_gateway.gateway.backend_address_pool.backend_pool.id]
     }
   }
 
