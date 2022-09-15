fc85e888ffe49e682b5073459cbae15eb566ef87
Simplon-Luna <108002178+Simplon-Luna@users.noreply.github.com>
2022-09-15 12:08:12 +0200

    c'est bon?

--- main.tf
+++ main.tf
@@ -198,39 +198,39 @@ resource "azurerm_bastion_host" "bastion" {
 #**#   address_prefixes     = ["10.0.2.0/24"]
 #**# }
 #**# 
-#**#     # Creation Nic App
-#**# resource "azurerm_network_interface" "nic_app" {
-#**#   name                = "${var.prefix}nic_app"
-#**#   location            = azurerm_resource_group.rg.location
-#**#   resource_group_name = azurerm_resource_group.rg.name
-#**# 
-#**#   ip_configuration {
-#**#     name                          = "${var.prefix}nic_app_config"
-#**#     subnet_id                     = azurerm_subnet.subnet_app.id
-#**#     private_ip_address_allocation = "Dynamic"
-#**#   }
-#**# }
-#**# 
-#**#     # Creation NSG App
-#**# resource "azurerm_network_security_group" "nsg_app" {
-#**#   name                = "${var.prefix}nsg_app"
-#**#   location            = azurerm_resource_group.rg.location
-#**#   resource_group_name = azurerm_resource_group.rg.name
-#**# 
-#**#   security_rule {
-#**#     name                       = "HTTP"
-#**#     priority                   = 100
-#**#     direction                  = "Inbound"
-#**#     access                     = "Allow"
-#**#     protocol                   = "Tcp"
-#**#     source_port_range          = "*"
-#**#     destination_port_range     = "80"
-#**#     source_address_prefix      = "*"
-#**#     destination_address_prefix = "*"
-#**#   }
-#**# }
-#**# 
-#**#     # Association NIC & NSG
+    # Creation Nic App
+resource "azurerm_network_interface" "nic_app" {
+  name                = "${var.prefix}nic_app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.prefix}nic_app_config"
+    subnet_id                     = azurerm_subnet.subnet_app.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+   # Creation NSG App
+resource "azurerm_network_security_group" "nsg_app" {
+  name                = "${var.prefix}nsg_app"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+    # Association NIC & NSG
 #**# resource "azurerm_network_interface_security_group_association" "assoc-nic-nsg-app" {
 #**#   network_interface_id      = azurerm_network_interface.nic_app.id
 #**#   network_security_group_id = azurerm_network_security_group.nsg_app.id
